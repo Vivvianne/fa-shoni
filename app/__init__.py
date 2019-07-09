715549880293f18c9cc5e8b441197fdeec564993
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-09 17:28:22 +0300

    added a picture.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+# Initializing application
+app = Flask(__name__,instance_relative_config = True)
 
-app = Flask(__name__)
+# Setting up configuration
 
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 from app.main import views
