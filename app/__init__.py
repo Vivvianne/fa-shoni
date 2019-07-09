c4d4bba1c14632fcb93ce8c1274e7733436c03ec
feven <feven.m.woldu@gmail.com>
2019-07-09 15:13:54 +0300

    installed bootstrap and and displayed my sample navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
 
 def create_app():
     app = Flask(__name__)
 
+    # Initializing Flask Extensions
+    bootstrap.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    
+
     return app
