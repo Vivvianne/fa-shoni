4565c0ab16228ad4923bccf91ab0103d964b38c0
feven <feven.m.woldu@gmail.com>
2019-07-10 15:57:20 +0300

    added a route for the landing page index.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,29 @@
+from flask import render_template,request,redirect,url_for
 from . import main
-from flask import render_template
+from ..models import User,Subscription
+from .. import db
+from .forms import SubscriptionForm
+from ..email import mail_message
+
+
+
+
 # Views
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
     '''
     View root page function that returns the index page and its data
     '''
 
-    title = 'Home - Welcome to fa-shoni'
+    form = SubscriptionForm()
 
+   
 
-    return render_template('index.html', title=title)
+    if form.validate_on_submit(): 
+               
+        new_subscription = Subscription(email=form.email.data)
+        new_subscription.save_email()
+
+    
+
+    return render_template('index.html', SubscriptionForm = form)
