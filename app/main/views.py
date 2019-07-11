7db0168403cfdd76651e904238a85b29c0fb81c5
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-11 08:05:46 +0300

    added some javascript codes.

--- app/main/views.py
+++ app/main/views.py
@@ -4,6 +4,35 @@ from app import app
 @app.route('/')
 def index():
     '''
-    View the home page
+    View the home page (to be added from landing page syntax)
     '''
-    return render_template('children.html')
+    
+    # children =b.get_b('children')
+    # men =b.get_b('men')
+    # women =b.get_b('women')
+    return render_template('cart.html')
+# @main.route('/b/children')
+# def children():
+#     b=b.get_b('children')
+#     return render_template('children.html',b=b)
+# @main.route('/b/men')
+# def men():
+#     b = b.get_b('men')
+#     return render_template('men',b=b)
+# @main.router('/b/women')
+# def women():
+#     b = b.get_b('women')
+#     return render_template('women',b=b)
+    
+# @app.route("/cart/add", methods=['POST'])
+# def add_to_cart():
+#     cart = ShoppingCart.add(product=request.form['product'], quantity=int(request.form['quantity']))
+#     return jsonify(cart)
+# @app.route("/cart")
+# def view_cart():
+#     cart = ShoppingCart.get()
+#     return render_template("cart.html", cart=cart)
+# @app.route("/cart/remove/<item_id>", methods=['POST'])
+# def remove_from_cart(item_id):
+#     cart = ShoppingCart.remove(item_id)
+#     return jsonify(cart)
